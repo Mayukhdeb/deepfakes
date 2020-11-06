04f490c3ecb235c7344be087ccd8004ab579c042
mayukhdeb <mayukhmainak2000@gmail.com>
2020-11-06 20:41:07 +0530

    crop_video_faces: remove face_alignment

--- deepcake/crop_video_faces.py
+++ deepcake/crop_video_faces.py
@@ -1,33 +1,22 @@
 import os 
+
 import  cv2
+
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import shutil
 import matplotlib.pyplot as plt
-import face_alignment
 
-
-def find_landmarks(image_np):
-    preds = fa.get_landmarks(image_np)[0]
-    x = preds[:,0]
-    y = preds[:,1]
-    
-    d = {
-        "x": x,
-        "y": y
-    }
-    return d
 
 current_folder = os.path.dirname(__file__)
-
-fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False, device = "cuda")
+haarcascade_path = current_folder + "/" + "haarcascade_frontalface_default.xml"
+face_cascade = cv2.CascadeClassifier( haarcascade_path)
 
 def trim_video(source_path, start_time, end_time, target_path):
     ffmpeg_extract_subclip(source_path, start_time, end_time, targetname=target_path)
 
 
 
-def crop_video_faces(video_path, new_folder_path, size = (64,64), pad = 0):
-
-    print("padding set to ", pad)
+def crop_video_faces(video_path, new_folder_path, size = (64,64)):
     frames_paths = []
     try:
         os.mkdir(new_folder_path)
@@ -40,34 +29,35 @@ def crop_video_faces(video_path, new_folder_path, size = (64,64), pad = 0):
 
     while success: 
         success, image = vidObj.read() 
+        fmt_name = new_folder_path + "/" + str(count)+ ".jpg"
+        try:
 
-        if success == 0:
-            pass
-        else:
-            fmt_name = new_folder_path + "/" + str(count)+ ".jpg"
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray = cv2.bilateralFilter(gray,5,1,1)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5,minSize=(50,50))
 
-            try:
-                land_marks = find_landmarks(image)
+            for (x, y, w, h) in faces:
+                image = image[y:y+h, x:x+w, :]
+            
+            image = cv2.resize(image,size)
+            
+            if len(faces) != 0:
+                cv2.imwrite(fmt_name, image)
+                frames_paths.append(fmt_name)
 
-                if land_marks is not None:
-
-                    x = int(land_marks["x"].min())
-                    y = int(land_marks["y"].min())
-
-                    w  = int((land_marks["x"].max() - land_marks["x"].min()))
-
-                    h  = int((land_marks["y"].max() - land_marks["y"].min()))
-
-                    image = image[y - pad :y+h + pad, x - pad :x+w + pad , :]
-
-                    image = cv2.resize(image, size)
-
-                    cv2.imwrite(fmt_name, image)
-                    count += 1
-                    if count % 100 == 0:
-                        print(count)
-
-            except KeyboardInterrupt:
-                break
-
+                count += 1
+                if count % 100 == 0:
+                    print(count)
+        except:
+            pass 
     print ("saved ", count-1, " frames at ", new_folder_path)
+    return frames_paths
+
+
+#print("main")
+
+#trim_video(source_path = "datasets/obama.mp4", start_time = 60 + 52, end_time = 120 + 5, target_path = "datasets/obama_trimmed.mp4")
+#video_to_frames("datasets/obama_trimmed.mp4", "datasets/obama")
+
+# trim_video(source_path = "datasets/trump.mp4", start_time = 2, end_time = 60, target_path = "datasets/trump_trimmed.mp4")
+# video_to_frames("datasets/trump_trimmed.mp4", "datasets/trump"
